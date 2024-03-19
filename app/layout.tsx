7e07ca4d1a140a2930ee1b925d51be24784d8e51
百倍 <285679784@qq.com>
2024-03-19 10:45:34 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "小助手WorkBot",
+  description: "由精益通团队构建的GPT聊天机器人.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "小助手WorkBot",
     statusBarStyle: "default",
   },
 };
